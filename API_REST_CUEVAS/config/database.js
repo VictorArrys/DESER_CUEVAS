9e19d3dfba61bad9978a03d8afbb518e48aae80a
AbrahamCRV <danielcrv7@outlook.com1>
2022-12-06 17:59:53 -0600

    arreglo de bugs menores

--- API_REST_CUEVAS/config/database.js
+++ API_REST_CUEVAS/config/database.js
@@ -10,19 +10,34 @@ const sql = require ('mssql')
 });*/
 
 const configuracionDB = {
-    user: "sa",
-    password: "usagitsukino",
-    server: "localhost",
-    database: "SistemaReportesVehiculos",
-    port: 1433,
+    user: "equipo3",
+    password: "UndercoverA07",
+    server: "servercuevas.database.windows.net",
+    database: "bdcuevas",
+    //port: 1433,
     options: {
-        encrypt: false, 
+        encrypt: true, 
+        trustServerCertificate: true,
+        enableArithAbrot: true,
+        trustedconnection: false,
+        instancename: 'servercuevas'
+    },
+}
+
+/*const configuracionDB = {
+    user: "sa",
+    password: "123456",
+    server: "X2-PCX1\SQLSERVER",
+    database: "bdcuevas",
+    //port: 1433,
+    options: {
+        encrypt: true, 
         trustServerCertificate: true,
         enableArithAbrot: true,
         trustedconnection: false,
         instancename: 'LOCALHOST'
     },
-}
+}*/
 
 
 module.exports = configuracionDB;
