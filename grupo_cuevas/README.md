7837898216010d6e00756de54895ebe25b6760b0
AbrahamCRV <danielcrv7@outlook.com1>
2022-12-12 14:21:38 -0600

    implementación login en proceso

--- grupo_cuevas/README.md
+++ grupo_cuevas/README.md
@@ -68,3 +68,12 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+
+////////////////////////////////////////////////////
+instalaciones necesarias para correr la app
+-npm install react-scripts
+-yarn add @material-ui/core
+-yarn add @material-ui/icons
--- grupo_cuevas/src/App.js
+++ grupo_cuevas/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {Grid} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import fondo from './componentes/fondo.png'
+
+const estilo = makeStyles(theme=> ({
+  root: {
+    backgroundImage: `url(${fondo})`
+  }
+}));
 
 function App() {
+  const clases = estilo()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container component='main' className={clases.root}>
+      hola
+    </Grid>
   );
 }
 
