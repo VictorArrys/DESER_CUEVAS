e634b4fa6cf8a100bd1be893b87f04889d36c1ff
VictorArrys <vctmanuel.arrys@gmail.com>
2022-12-13 21:43:11 -0600

    Modificacion de login

--- frontend/src/componentes/Login/Login.jsx
+++ frontend/src/componentes/Login/Login.jsx
@@ -3,15 +3,38 @@ import  "./login.css"
 
 export const Login = () => {
   return (
-            <div class="overlay">
-                <form >
+          <div class="container">
+            <div class="screen">
+		          <div class="screen__content">                    
+                <form class = "login">
+                  
+                  <div class="login__field">
+                    <i class="login__icon fas fa-user"></i>
+                    <input type="text" class="login__input" placeholder="Email" />
+                  </div>      
+                  
+                  <div class="login__field">
+                    <i class="login__icon fas fa-lock"></i>
+                    <input type="password" class="login__input" placeholder="Password" />
+                  </div>
 
-                    <h2> Iniciar sesion </h2>
-                    <p type="Email:"><input class="form-input" placeholder="Ingrese su correo.."></input></p>
-                    <p type="Password:"><input class="form-input" type="password" placeholder="Clave.."></input></p>
-                    <button>Ingresar</button>
-                </form>
+                  <button class="button login__submit">
+                    <span class="button__text">Log In Now</span>
+                    <i class="button__icon fas fa-chevron-right"></i>
+                  </button>			
+
+                </form>                
+
+              </div>
+
+              <div class="screen__background">
+                  <span class="screen__background__shape screen__background__shape4"></span>
+                  <span class="screen__background__shape screen__background__shape3"></span>		
+                  <span class="screen__background__shape screen__background__shape2"></span>
+                  <span class="screen__background__shape screen__background__shape1"></span>
+              </div>	
 
             </div> 
+          </div>
   );
 }
--- frontend/src/componentes/Login/Menu.jsx
+++ frontend/src/componentes/Login/Menu.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import  "./login.css"
 
-export const Menu = () => {
+export const MenuPrincipal = (Componente) => {
   return (
-    <aside class="sidebar">
-    <nav class="nav">
-      <ul>
-        <li class="active">Welcome</li>
-      </ul>
-    </nav>
-  </aside>
+    <div class="containerMenu">
+      <nav>
+        <a href="/abarrotes_cuevas/productos">Option 1</a>
+        <a href=".">Option 2</a>
+        <a href=".">Option 3</a>
+        <a href=".">Option 4</a>
+      </nav>     
+      <Componente />
+
+    </div>
+
   )}
