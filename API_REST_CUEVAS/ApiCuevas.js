5510364df91ace6d20034dfeba51881efc3e6c27
VictorArrys <vctmanuel.arrys@gmail.com>
2022-12-14 20:21:58 -0600

    Se agrega la libreria cors para permitir conexion a la API

--- API_REST_CUEVAS/ApiCuevas.js
+++ API_REST_CUEVAS/ApiCuevas.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 const express = require('express');
 const req = require('express/lib/request');
+const cors = require('cors')
 const app = express();
 const morgan = require('morgan');
 const port = process.env.PORT || 3000;
@@ -8,10 +9,10 @@ const port = process.env.PORT || 3000;
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}))
 app.use(express.json())
+app.use(cors());
 
 //routes
 app.use('/api/abarrotes_cuevas/1.0', require('./routes'))
-
 
 /*
  Todo: Ejecucion del servidor
